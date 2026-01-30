e62cb8f43b243c4573757037e55a0cc6f9d7f8cb
Sethuram <k.v.sethuram31@gmail.com>
2026-01-30 21:51:15 +0530

    Basic Page Routing Done

---
+++ app/activity-log/page.tsx
@@ -0,0 +1,3 @@
+export default function ActivityLog() {
+  return <>Hello Welcome to Activity Page</>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
 "use client";
 
-import { Typography } from "@mui/material";
+import { Typography, Button } from "@mui/material";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return <Typography variant="h2" align="center" mt={1}>Frontend performance-focused activity feed </Typography>;
+  const router = useRouter();
+  return (
+    <>
+      <Typography variant="h2" align="center" mt={1}>
+        Frontend performance-focused activity feed{" "}
+      </Typography>
+      Click the Button to Navigate to Activity Log Page
+      <Button
+        onClick={() => {
+          router.push("/activity-log");
+        }}
+      >
+        Click Here
+      </Button>
+    </>
+  );
 }
